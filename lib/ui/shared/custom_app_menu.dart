ef644642ac8fd9595f07a7a5254422ef19e81cb2
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2023-10-02 11:18:32 -0500

    UPDATE

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:vertical_landing_page/providers/page_provider.dart';
@@ -11,28 +10,25 @@ class CustomAppMenu extends StatefulWidget {
 }
 
 class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProviderStateMixin {
-
   bool isOpen = false;
   late AnimationController controller;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
-    
-    controller = AnimationController(vsync: this, duration: Duration( milliseconds: 200 ));
+
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 200));
   }
 
   @override
   Widget build(BuildContext context) {
-
     final pageProvider = Provider.of<PageProvider>(context, listen: false);
 
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
-        onTap: (){ 
-          
-          if ( isOpen ) {
+        onTap: () {
+          if (isOpen) {
             controller.reverse();
           } else {
             controller.forward();
@@ -41,26 +37,23 @@ class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProvider
           setState(() {
             isOpen = !isOpen;
           });
-
         },
         child: Container(
-          padding: EdgeInsets.symmetric( horizontal: 10 ),
+          padding: EdgeInsets.symmetric(horizontal: 10),
           width: 150,
-          height: isOpen ? 308: 50,
+          height: isOpen ? 308 : 50,
           color: Colors.black,
           child: Column(
             children: [
               _MenuTitle(isOpen: isOpen, controller: controller),
-
-              if ( isOpen )
-                ...[
-                  CustomMenuItem( delay: 0, text: 'Home', onPressed: () => pageProvider.goTo(0)),
-                  CustomMenuItem( delay: 30, text: 'About', onPressed: () => pageProvider.goTo(1)),
-                  CustomMenuItem( delay: 60, text: 'Pricing', onPressed: () => pageProvider.goTo(2)),
-                  CustomMenuItem( delay: 90, text: 'Contact', onPressed: () => pageProvider.goTo(3)),
-                  CustomMenuItem( delay: 120, text: 'Location', onPressed: () => pageProvider.goTo(4)),
-                  SizedBox( height: 8 )
-                ]
+              if (isOpen) ...[
+                CustomMenuItem(delay: 0, text: 'Home', onPressed: () => pageProvider.goTo(0)),
+                CustomMenuItem(delay: 30, text: 'About', onPressed: () => pageProvider.goTo(1)),
+                CustomMenuItem(delay: 60, text: 'Pricing', onPressed: () => pageProvider.goTo(2)),
+                CustomMenuItem(delay: 90, text: 'Contact', onPressed: () => pageProvider.goTo(3)),
+                CustomMenuItem(delay: 120, text: 'Location', onPressed: () => pageProvider.goTo(4)),
+                SizedBox(height: 8)
+              ]
             ],
           ),
         ),
@@ -68,9 +61,6 @@ class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProvider
     );
   }
 }
-
-
-
 
 class _MenuTitle extends StatelessWidget {
   const _MenuTitle({
@@ -90,15 +80,15 @@ class _MenuTitle extends StatelessWidget {
       child: Row(
         children: [
           AnimatedContainer(
-            duration: Duration( milliseconds: 200 ),
+            duration: Duration(milliseconds: 200),
             curve: Curves.easeInOut,
             width: isOpen ? 45 : 0,
           ),
-          Text('Menú', style: GoogleFonts.roboto( color: Colors.white, fontSize: 18 )),
+          Text('Menú', style: GoogleFonts.roboto(color: Colors.white, fontSize: 18)),
           Spacer(),
           AnimatedIcon(
             icon: AnimatedIcons.menu_close,
-            progress: controller, 
+            progress: controller,
             color: Colors.white,
           )
         ],
